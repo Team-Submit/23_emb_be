df483b6c139e2857062b93cf78bc16f52e2ec97b
sim <simyechan87@gmail.com>
2023-10-24 17:08:04 +0900

    :sparkles: 로그아웃 기능 추가

--- emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
+++ emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
@@ -1,7 +1,11 @@
 package com.example.emb.domain.user.presentation;
 
+import com.example.emb.domain.user.service.UserLogoutService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private final UserLogoutService userLogoutService;
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    public void logout() {
+        userLogoutService.execute();
+    }
 }
