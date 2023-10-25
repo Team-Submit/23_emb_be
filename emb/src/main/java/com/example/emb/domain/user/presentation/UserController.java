fe5a51c3bf9711d42aa63951dcd9e21c7996ae36
양희범 <ay612@dsm.hs.kr>
2023-10-25 11:03:21 +0900

    :최초로그인 기능 추가

--- emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
+++ emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
@@ -1,12 +1,25 @@
 package com.example.emb.domain.user.presentation;
 
+import com.example.emb.domain.user.facade.dto.UserSignUpRequest;
+import com.example.emb.domain.user.service.UserSignUpService;
+import io.swagger.annotations.ApiOperation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/users")
 @RestController
+@RequestMapping
 public class UserController {
 
+    private final UserSignUpService userSignUpService;
+
+
+    @ApiOperation(value = "첫 로그인")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/firstLogin")
+    public void userSignUp(@RequestBody @Valid UserSignUpRequest request) {
+        userSignUpService.execute(request);
+    }
 }
