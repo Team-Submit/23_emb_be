7c4149eb9cba36d034747cf2226e0d649640085a
양희범 <ay612@dsm.hs.kr>
2023-10-26 09:24:52 +0900

    :recycle: 애너테이션 수정

--- emb/src/main/java/com/example/emb/domain/user/facade/dto/UserSignUpRequest.java
+++ emb/src/main/java/com/example/emb/domain/user/facade/dto/UserSignUpRequest.java
@@ -1,7 +1,9 @@
 package com.example.emb.domain.user.facade.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -11,23 +13,23 @@ import org.hibernate.validator.constraints.Length;
 public class UserSignUpRequest {
 
     @ApiModelProperty(value = "name", example = "홍길동")
-    @NotNull
-    @Length(max = 50)
+    //@NotNull(message = "필드는 비어 있을 수 없습니다.")
+    @Length(max = 50, message = "필드는 {max} 글자 이하여야 합니다.")
     private String userName;
 
     @ApiModelProperty(value = "password", example = "aced1234")
-    @NotNull
-    @Length(max = 30)
+    //@NotNull(message = "필드는 비어 있을 수 없습니다.")
+    @Length(min = 8, max = 30, message = "필드는 {min} 글자 이상 {max} 글자 이하여야 합니다.")
     private String newPassword;
 
     @ApiModelProperty(value = "phoneNumber", example = "01011112222")
-    @NotNull
-    @Length(max = 20)
+    //@NotNull(message = "필드는 비어 있을 수 없습니다.")
+    @Length(max = 255, message = "필드는 {max} 글자 이하여야 합니다.")
     private String userNumber;
 
     @ApiModelProperty(value = "department", example = "행복안전부")
-    @NotNull
-    @Length(max = 255)
+    //@NotNull(message = "필드는 비어 있을 수 없습니다.")
+    @Length(min = 2, max = 255)
     private String department;
 
 }
