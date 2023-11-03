38ef7bec49ce83eb128b3b03977a8cb7c7c3ff7c
sim <simyechan87@gmail.com>
2023-11-03 16:19:19 +0900

    ✨ 로그아웃 기능 추가

--- emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
+++ emb/src/main/java/com/example/emb/domain/user/presentation/UserController.java
@@ -1,6 +1,7 @@
 package com.example.emb.domain.user.presentation;
 
 import com.example.emb.domain.user.facade.dto.UserSignUpRequest;
+import com.example.emb.domain.user.service.UserLogoutService;
 import com.example.emb.domain.user.service.UserSignUpService;
 import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
@@ -14,11 +15,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserSignUpService userSignUpService;
+    private final UserLogoutService userLogoutService;
+
     @ApiOperation(value = "첫 로그인")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/firstLogin")
 
     public void userSinUp(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    public void logout() {
+        userLogoutService.execute();
     }
 }
