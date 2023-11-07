6ec2d7a1b6a5e47f34bebea900531cab1683fbc4
sim <simyechan87@gmail.com>
2023-11-07 22:59:49 +0900

    ♻️ 코드 수정

--- emb/src/main/java/com/example/emb/domain/manager/presentation/ManagerController.java
+++ emb/src/main/java/com/example/emb/domain/manager/presentation/ManagerController.java
@@ -2,6 +2,7 @@ package com.example.emb.domain.manager.presentation;
 
 import com.example.emb.domain.auth.presentation.dto.response.UserTokenResponse;
 import com.example.emb.domain.manager.presentation.dto.request.ManagerSignUpRequest;
+import com.example.emb.domain.manager.service.AccountDeleteService;
 import com.example.emb.domain.manager.service.AccountSignUpService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class ManagerController {
 
     private final AccountSignUpService userSignUpService;
+    private final AccountDeleteService accountDeleteService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/accounts")
@@ -24,6 +26,6 @@ public class ManagerController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/accounts/{user-id}")
     public void accountDelete(@PathVariable ("user-id") String userId) {
-        userSignUpService.execute(userId);
+        accountDeleteService.execute(userId);
     }
 }
