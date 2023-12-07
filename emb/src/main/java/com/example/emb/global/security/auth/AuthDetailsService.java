e069e723427c1a293bc8b5134b01ec2c08c81724
sim <simyechan87@gmail.com>
2023-12-08 07:46:27 +0900

    :recycle: Manager 404오류 수정

--- emb/src/main/java/com/example/emb/global/security/auth/AuthDetailsService.java
+++ emb/src/main/java/com/example/emb/global/security/auth/AuthDetailsService.java
@@ -1,5 +1,7 @@
 package com.example.emb.global.security.auth;
 
+import com.example.emb.domain.manager.domain.Manager;
+import com.example.emb.domain.manager.domain.repository.ManagerRepository;
 import com.example.emb.domain.user.domain.repository.UserRepository;
 import com.example.emb.global.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class AuthDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final ManagerRepository managerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String user_id) throws UsernameNotFoundException {
-        return userRepository.findByUserId(user_id)
-                .map(AuthDetails::new)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        Optional<Manager> managerOptional = managerRepository.findById(user_id);
+
+        if (managerOptional.isPresent()) {
+            return managerRepository.findById(user_id)
+                    .map(ManagerAuthDetails::new)
+                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        } else {
+            return userRepository.findByUserId(user_id)
+                    .map(AuthDetails::new)
+                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        }
     }
 }
