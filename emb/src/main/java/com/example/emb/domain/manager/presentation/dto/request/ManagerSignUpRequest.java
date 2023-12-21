cf88335794480eb016015aa9b17d0525ab5afdab
sim <simyechan87@gmail.com>
2023-12-21 21:18:44 +0900

    ♻️ 비밀번호 영어, 숫자, 특수문자 1개이상 수정

--- emb/src/main/java/com/example/emb/domain/manager/presentation/dto/request/ManagerSignUpRequest.java
+++ emb/src/main/java/com/example/emb/domain/manager/presentation/dto/request/ManagerSignUpRequest.java
@@ -1,6 +1,7 @@
 package com.example.emb.domain.manager.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -13,6 +14,7 @@ public class ManagerSignUpRequest {
     private  String id;
 
     @NotBlank(message = "password는 Null, 공백을 허용하지 않습니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!])(?=\\S+$).*$", message = "비밀번호는 영어, 숫자, 특수문자를 포함해야 합니다.")
     @Length(min = 8, max = 255, message = "password는 30글자 이하여야 합니다.")
     private String password;
 }
--- emb/src/main/java/com/example/emb/domain/user/presentation/dto/request/UserSignUpRequest.java
+++ emb/src/main/java/com/example/emb/domain/user/presentation/dto/request/UserSignUpRequest.java
@@ -2,6 +2,7 @@ package com.example.emb.domain.user.presentation.dto.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -17,6 +18,7 @@ public class UserSignUpRequest {
 
     @ApiModelProperty(value = "password", example = "aced1234")
     @NotNull(message = "필드는 비어 있을 수 없습니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!])(?=\\S+$).*$", message = "비밀번호는 영어, 숫자, 특수문자를 포함해야 합니다.")
     @Length(min = 8, max = 30, message = "필드는 {min} 글자 이상 {max} 글자 이하여야 합니다.")
     private String newPassword;
 
